4a63e586d25c85c049bb940fb5cc199a71e07b80
Osqqiw <oscartrobat@hotmail.com>
2024-08-03 18:23:01 -0300

    Add files via upload

---
+++ EntregaTrabajo/Aplicacion/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .models import Curso, Alumnos, Trabajos_Practicos
+from .forms import CursoForm, AlumnoForm, TrabajoPracticoForm
+
+def listar_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'aplicacion/listar_cursos.html', {'cursos': cursos})
+
+def nuevo_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_cursos')
+    else:
+        form = CursoForm()
+    return render(request, 'aplicacion/nuevo_curso.html', {'form': form})
+
+def listar_alumnos(request):
+    alumnos = Alumnos.objects.all()
+    return render(request, 'aplicacion/listar_alumnos.html', {'alumnos': alumnos})
+
+def nuevo_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_alumnos')
+    else:
+        form = AlumnoForm()
+    return render(request, 'aplicacion/nuevo_alumno.html', {'form': form})
+
+def listar_trabajos_practicos(request):
+    trabajos_practicos = Trabajos_Practicos.objects.all()
+    return render(request, 'aplicacion/listar_trabajos_practicos.html', {'trabajos_practicos': trabajos_practicos})
+
+def nuevo_trabajo_practico(request):
+    if request.method == 'POST':
+        form = TrabajoPracticoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_trabajos_practicos')
+    else:
+        form = TrabajoPracticoForm()
+    return render(request, 'aplicacion/nuevo_trabajo_practico.html', {'form': form})
